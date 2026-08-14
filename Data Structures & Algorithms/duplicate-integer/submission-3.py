cc87e98ca7fc833b598f63346ee2afb9af15601d
Mohammed Ibrahim Faheem <140500973+Faheemibrahim@users.noreply.github.com>
2026-08-14 23:31:07 +1000

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        set1 = set()
+
+        for num in nums:
+            set1.add(num)
+
+        set_size = len(set1)
+        list_size = len(nums)
+
+        if set_size < list_size:
+            return True
+        else:
+            return False
+
+            
